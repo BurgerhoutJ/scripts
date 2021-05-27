f45ac5f12219445f7b9fde9dacb8fdca5ae92b34
Jeroen Burgerhout <jeroen@burgerhout.org>
2021-05-27 19:08:51 +0200

    Update CreatingTeamsRoomAccount.ps1

--- microsoft-teams-rooms/CreatingTeamsRoomAccount.ps1
+++ microsoft-teams-rooms/CreatingTeamsRoomAccount.ps1
@@ -1,25 +1,15 @@
-﻿# Connect to Exchange Online and create a resource
+# Connect to Exchange Online and create a resource
 Set-ExecutionPolicy RemoteSigned
+Import-Module ExchangeOnlineManagement
 $UserCredential = Get-Credential
-$Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://outlook.office365.com/powershell-liveid/ -Credential $UserCredential -Authentication Basic -AllowRedirection
-Import-PSSession $Session -DisableNameChecking
-New-Mailbox -Name "MTR-ConferenceRoom02" -Alias ConferenceRoom02 -Room -EnableRoomMailboxAccount $true -MicrosoftOnlineServicesID MTR-ConferenceRoom02@theorange.cat -RoomMailboxPassword (ConvertTo-SecureString -String '>4#nU+QX^eFHB&fzn}qw' -AsPlainText -Force)
-Set-CalendarProcessing -Identity "MTR-ConferenceRoom02" -AutomateProcessing AutoAccept -AddOrganizerToSubject $false -DeleteComments $false -DeleteSubject $false -RemovePrivateProperty $false -AddAdditionalResponse $true -AdditionalResponse "This is a Microsoft Teams Room!"
+$Session = Connect-ExchangeOnline -UserPrincipalName $UserCredential -Credential $UserCredential
+New-Mailbox -Name "MTR-TeamsRoom" -Alias TeamsRoom -Room -EnableRoomMailboxAccount $true -MicrosoftOnlineServicesID MTR-TeamsRoom@theorange.cat -RoomMailboxPassword (ConvertTo-SecureString -String '<password>' -AsPlainText -Force)
+Set-CalendarProcessing -Identity "MTR-TeamsRoom" -AutomateProcessing AutoAccept -AllowConflicts $false -AddOrganizerToSubject $false -DeleteComments $false -DeleteSubject $false -RemovePrivateProperty $false -AddAdditionalResponse $true -AdditionalResponse "This is a Microsoft Teams Room!"
 
 #Sleep for 20 seconds
 Start-Sleep 20
 
 #Connect to MS Online to set password to never expires
 Connect-MsolService -Credential $UserCredential
-Set-MsolUser -UserPrincipalName MTR-ConferenceRoom02@theorange.cat -PasswordNeverExpires $true
-Get-MsolUser -UserPrincipalName MTR-ConferenceRoom02@theorange.cat | Select PasswordNeverExpires
-
-#Sleep for 300 seconds to populate the security group and assign a SfB account
-Start-Sleep 300
-
-#Enable account for SfB
-Import-Module SkypeOnlineConnector
-$cssess=New-CsOnlineSession -Credential $UserCredential
-Import-PSSession $cssess -AllowClobber
-$rm="MTR-ConferenceRoom02@theorange.cat"
-Enable-CsMeetingRoom -Identity $rm -RegistrarPool "sippoolDB42E12.infra.lync.com" -SipAddressType EmailAddress
+Set-MsolUser -UserPrincipalName MTR-TeamsRoom@theorange.cat -PasswordNeverExpires $true
+Get-MsolUser -UserPrincipalName MTR-TeamsRoom@theorange.cat | Select PasswordNeverExpires
