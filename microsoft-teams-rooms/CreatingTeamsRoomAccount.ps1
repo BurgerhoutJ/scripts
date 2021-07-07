498795eb8f3901abcdef01769eaf7a3eca89ba16
Jeroen Burgerhout <jeroen@burgerhout.org>
2021-07-07 10:18:17 +0200

    Update CreatingTeamsRoomAccount.ps1

--- microsoft-teams-rooms/CreatingTeamsRoomAccount.ps1
+++ microsoft-teams-rooms/CreatingTeamsRoomAccount.ps1
@@ -3,7 +3,7 @@ Set-ExecutionPolicy RemoteSigned
 Import-Module ExchangeOnlineManagement
 $UserCredential = Get-Credential
 $Session = Connect-ExchangeOnline -UserPrincipalName $UserCredential -Credential $UserCredential
-New-Mailbox -Name "MTR-TeamsRoom" -Alias TeamsRoom -Room -EnableRoomMailboxAccount $true -MicrosoftOnlineServicesID MTR-TeamsRoom@theorange.cat -RoomMailboxPassword (ConvertTo-SecureString -String '<password>' -AsPlainText -Force)
+New-Mailbox -Name "MTR-TeamsRoom" -Alias TeamsRoom -Room -EnableRoomMailboxAccount $true -UserPrincipalName MTR-TeamsRoom@theorange.cat -RoomMailboxPassword (ConvertTo-SecureString -String '<password>' -AsPlainText -Force)
 Set-CalendarProcessing -Identity "MTR-TeamsRoom" -AutomateProcessing AutoAccept -AllowConflicts $false -AddOrganizerToSubject $false -DeleteComments $false -DeleteSubject $false -RemovePrivateProperty $false -AddAdditionalResponse $true -AdditionalResponse "This is a Microsoft Teams Room!"
 
 #Sleep for 20 seconds
