d51d6ce63068fe556c3e8082e2a70f8aafc860a4
Jeroen Burgerhout <jeroen@burgerhout.org>
2021-11-26 10:35:14 +0100

    Update CreatingTeamsRoomAccount.ps1
    Microsoft changed some Powershell cmdlets again.

--- microsoft-teams-rooms/CreatingTeamsRoomAccount.ps1
+++ microsoft-teams-rooms/CreatingTeamsRoomAccount.ps1
@@ -1,15 +1,15 @@
 # Connect to Exchange Online and create a resource
+# Connect to Exchange Online and create a resource
 Set-ExecutionPolicy RemoteSigned
-Import-Module ExchangeOnlineManagement
-$UserCredential = Get-Credential
-$Session = Connect-ExchangeOnline -UserPrincipalName $UserCredential -Credential $UserCredential
-New-Mailbox -Name "MTR-TeamsRoom" -Alias TeamsRoom -Room -EnableRoomMailboxAccount $true -UserPrincipalName MTR-TeamsRoom@theorange.cat -RoomMailboxPassword (ConvertTo-SecureString -String '<password>' -AsPlainText -Force)
-Set-CalendarProcessing -Identity "MTR-TeamsRoom" -AutomateProcessing AutoAccept -AllowConflicts $false -AddOrganizerToSubject $false -DeleteComments $false -DeleteSubject $false -RemovePrivateProperty $false -AddAdditionalResponse $true -AdditionalResponse "This is a Microsoft Teams Room!"
+Install-Module ExchangeOnlineManagement
+$Session = Connect-ExchangeOnline
+New-Mailbox -Name "MTR-TeamsRoom-Test" -Alias TeamsRoomTest -Room -EnableRoomMailboxAccount $true -MicrosoftOnlineServicesID MTR-TeamsRoom-Test@theorange.cat -RoomMailboxPassword (ConvertTo-SecureString -String 'Nieuwk00p' -AsPlainText -Force)
+Set-CalendarProcessing -Identity "MTR-TeamsRoom-Test" -AutomateProcessing AutoAccept -AllowConflicts $false -AddOrganizerToSubject $false -DeleteComments $false -DeleteSubject $false -RemovePrivateProperty $false -AddAdditionalResponse $true -AdditionalResponse "This is a Test Microsoft Teams Room!"
 
 #Sleep for 20 seconds
 Start-Sleep 20
 
 #Connect to MS Online to set password to never expires
-Connect-MsolService -Credential $UserCredential
-Set-MsolUser -UserPrincipalName MTR-TeamsRoom@theorange.cat -PasswordNeverExpires $true
-Get-MsolUser -UserPrincipalName MTR-TeamsRoom@theorange.cat | Select PasswordNeverExpires
+Connect-MsolService
+Set-MsolUser -UserPrincipalName MTR-TeamsRoom-Test@theorange.cat -PasswordNeverExpires $true
+Get-MsolUser -UserPrincipalName MTR-TeamsRoom-Test@theorange.cat | Select PasswordNeverExpires
