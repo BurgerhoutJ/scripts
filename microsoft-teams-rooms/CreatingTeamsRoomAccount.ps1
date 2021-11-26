d52080ecfea8098525f6d7fd6d6f3a3ec3dda5c2
Jeroen Burgerhout <jeroen@burgerhout.org>
2021-11-26 10:37:49 +0100

    Update CreatingTeamsRoomAccount.ps1
    Microsoft changed some Powershell cmdlets

--- microsoft-teams-rooms/CreatingTeamsRoomAccount.ps1
+++ microsoft-teams-rooms/CreatingTeamsRoomAccount.ps1
@@ -1,15 +1,14 @@
 # Connect to Exchange Online and create a resource
-# Connect to Exchange Online and create a resource
 Set-ExecutionPolicy RemoteSigned
 Install-Module ExchangeOnlineManagement
 $Session = Connect-ExchangeOnline
-New-Mailbox -Name "MTR-TeamsRoom-Test" -Alias TeamsRoomTest -Room -EnableRoomMailboxAccount $true -MicrosoftOnlineServicesID MTR-TeamsRoom-Test@theorange.cat -RoomMailboxPassword (ConvertTo-SecureString -String 'Nieuwk00p' -AsPlainText -Force)
-Set-CalendarProcessing -Identity "MTR-TeamsRoom-Test" -AutomateProcessing AutoAccept -AllowConflicts $false -AddOrganizerToSubject $false -DeleteComments $false -DeleteSubject $false -RemovePrivateProperty $false -AddAdditionalResponse $true -AdditionalResponse "This is a Test Microsoft Teams Room!"
+New-Mailbox -Name "MTR-TeamsRoom" -Alias TeamsRoom -Room -EnableRoomMailboxAccount $true -MicrosoftOnlineServicesID MTR-TeamsRoom@theorange.cat -RoomMailboxPassword (ConvertTo-SecureString -String '<password>' -AsPlainText -Force)
+Set-CalendarProcessing -Identity "MTR-TeamsRoom" -AutomateProcessing AutoAccept -AllowConflicts $false -AddOrganizerToSubject $false -DeleteComments $false -DeleteSubject $false -RemovePrivateProperty $false -AddAdditionalResponse $true -AdditionalResponse "This is a Microsoft Teams Room!"
 
 #Sleep for 20 seconds
 Start-Sleep 20
 
 #Connect to MS Online to set password to never expires
 Connect-MsolService
-Set-MsolUser -UserPrincipalName MTR-TeamsRoom-Test@theorange.cat -PasswordNeverExpires $true
-Get-MsolUser -UserPrincipalName MTR-TeamsRoom-Test@theorange.cat | Select PasswordNeverExpires
+Set-MsolUser -UserPrincipalName MTR-TeamsRoom@theorange.cat -PasswordNeverExpires $true
+Get-MsolUser -UserPrincipalName MTR-TeamsRoom@theorange.cat | Select PasswordNeverExpires
