1881abd9944d3d9702cd0a0ee933535702fdd175
these-work <141854953+these-work@users.noreply.github.com>
2023-08-09 12:27:43 -0400

    Update RemoveW10Bloatware.ps1
    This script will now give you a warning for each app in your $UninstallPackages list that isn't found either in the installed packages or the provisioned packages. Replaced all instances of Write-Host and Write-Warning with calls to Write-Log. As a result, only those specific messages will be written to the log file. It should be much cleaner now.

--- remove-bloatware/RemoveW10Bloatware.ps1
+++ remove-bloatware/RemoveW10Bloatware.ps1
@@ -2,15 +2,23 @@
 # Original file https://gist.github.com/mark05e/a79221b4245962a477a49eb281d97388#file-remove-hpbloatware-ps1 
 # and modified by Jeroen Burgerhout (@BurgerhoutJ)
 
+# Custom logging function
+function Write-Log {
+    param (
+        [Parameter(Mandatory=$true)]
+        [string] $Message
+    )
+
+    $logFilePath = "$($env:ProgramData)\Microsoft\RemoveW10Bloatware\RemoveW10Bloatware.log"
+    Add-Content -Path $logFilePath -Value "$(Get-Date -Format 'yyyy-MM-dd HH:mm:ss'): $Message"
+}
+
 # Create a tag file just so Intune knows this was installed
 if (-not (Test-Path "$($env:ProgramData)\Microsoft\RemoveW10Bloatware"))
 {
-    Mkdir "$($env:ProgramData)\Microsoft\RemoveW10Bloatware"
+    New-Item -ItemType Directory -Force -Path "$($env:ProgramData)\Microsoft\RemoveW10Bloatware" > $null
 }
 Set-Content -Path "$($env:ProgramData)\Microsoft\RemoveW10Bloatware\RemoveW10Bloatware.ps1.tag" -Value "Installed"
-
-# Start logging
-Start-Transcript "$($env:ProgramData)\Microsoft\RemoveW10Bloatware\RemoveW10Bloatware.log"
 
 # List of built-in apps to remove
 $UninstallPackages = @(
@@ -41,45 +49,54 @@ $UninstallPrograms = @(
 )
 
 $InstalledPackages = Get-AppxPackage -AllUsers | Where {($UninstallPackages -contains $_.Name)}
-
 $ProvisionedPackages = Get-AppxProvisionedPackage -Online | Where {($UninstallPackages -contains $_.DisplayName)}
-
 $InstalledPrograms = Get-Package | Where {$UninstallPrograms -contains $_.Name}
+
+# Print a warning for not found apps
+$UninstallPackages | ForEach {
+    if (($_ -notin $InstalledPackages.Name) -and ($_ -notin $ProvisionedPackages.DisplayName)) {
+        Write-Log -Message "Warning: App not found: [$_]"
+    }
+}
 
 # Remove provisioned packages first
 ForEach ($ProvPackage in $ProvisionedPackages) {
 
-    Write-Host -Object "Attempting to remove provisioned package: [$($ProvPackage.DisplayName)]..."
+    Write-Log -Message "Attempting to remove provisioned package: [$($ProvPackage.DisplayName)]..."
 
     Try {
-        $Null = Remove-AppxProvisionedPackage -PackageName $ProvPackage.PackageName -Online -ErrorAction Stop
-        Write-Host -Object "Successfully removed provisioned package: [$($ProvPackage.DisplayName)]"
+        Remove-AppxProvisionedPackage -PackageName $ProvPackage.PackageName -Online -ErrorAction Stop > $null
+        Write-Log -Message "Successfully removed provisioned package: [$($ProvPackage.DisplayName)]"
     }
-    Catch {Write-Warning -Message "Failed to remove provisioned package: [$($ProvPackage.DisplayName)]"}
+    Catch {
+        Write-Log -Message "Warning: Failed to remove provisioned package: [$($ProvPackage.DisplayName)]"
+    }
 }
 
 # Remove appx packages
 ForEach ($AppxPackage in $InstalledPackages) {
-                                            
-    Write-Host -Object "Attempting to remove Appx package: [$($AppxPackage.Name)]..."
+
+    Write-Log -Message "Attempting to remove Appx package: [$($AppxPackage.Name)]..."
 
     Try {
-        $Null = Remove-AppxPackage -Package $AppxPackage.PackageFullName -AllUsers -ErrorAction Stop
-        Write-Host -Object "Successfully removed Appx package: [$($AppxPackage.Name)]"
+        Remove-AppxPackage -Package $AppxPackage.PackageFullName -AllUsers -ErrorAction Stop > $null
+        Write-Log -Message "Successfully removed Appx package: [$($AppxPackage.Name)]"
     }
-    Catch {Write-Warning -Message "Failed to remove Appx package: [$($AppxPackage.Name)]"}
+    Catch {
+        Write-Log -Message "Warning: Failed to remove Appx package: [$($AppxPackage.Name)]"
+    }
 }
 
 # Remove installed programs
 $InstalledPrograms | ForEach {
 
-    Write-Host -Object "Attempting to uninstall: [$($_.Name)]..."
+    Write-Log -Message "Attempting to uninstall: [$($_.Name)]..."
 
     Try {
-        $Null = $_ | Uninstall-Package -AllVersions -Force -ErrorAction Stop
-        Write-Host -Object "Successfully uninstalled: [$($_.Name)]"
+        $_ | Uninstall-Package -AllVersions -Force -ErrorAction Stop > $null
+        Write-Log -Message "Successfully uninstalled: [$($_.Name)]"
     }
-    Catch {Write-Warning -Message "Failed to uninstall: [$($_.Name)]"}
+    Catch {
+        Write-Log -Message "Warning: Failed to uninstall: [$($_.Name)]"
+    }
 }
-
-Stop-Transcript
