f5938c950b1c94a28f729ab11f2ca8a68c82239a
Jeroen Burgerhout <jeroen@burgerhout.org>
2023-11-13 08:39:31 +0100

    Add files via upload

--- Detect-OutlookNew.ps1
+++ Detect-OutlookNew.ps1
@@ -11,4 +11,15 @@ Enforce script signature check: No
 Run script in 64-bit PowerShell: Yes
 #> 
 
-if (Get-AppxPackage -Name *OutlookForWindows*) {write-host "Microsoft Outlook (New) found."exit 1}else {write-host "Microsoft Outlook (New) not found."exit 0}
+if (Get-AppxPackage -Name *OutlookForWindows*) {
+write-host "Microsoft Outlook (New) found."
+
+exit 1
+
+}
+
+else {
+write-host "Microsoft Outlook (New) not found."
+
+exit 0
+}
