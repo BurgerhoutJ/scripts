4f24aaff0bcacf353cdc02efad1b627cb69137b7
Jeroen Burgerhout <jeroen@burgerhout.org>
2024-01-04 12:13:12 +0100

    Add files via upload

---
+++ remediation/Detect-DevHome.ps1
@@ -0,0 +1,24 @@
+<#
+Version: 1.0
+Author: 
+- Jeroen Burgerhout (burgerhout.org)
+Script: Detect-WindowsBackup
+Description: Script detects the new Dev Home app on Windows 11 23H2.
+Hint: This is a community script. There is no guarantee for this. Please check thoroughly before running.
+Version 1.0: Init
+Run this script using the logged-on credentials: Yes
+Enforce script signature check: No
+Run script in 64-bit PowerShell: Yes
+#> 
+
+if (Get-AppxPackage -Name *DevHome*) {
+write-host "Dev Home found."
+
+exit 1
+}
+
+else {
+write-host "Dev Home not found."
+
+exit 0
+}
---
+++ remediation/Remove-DevHome.ps1
@@ -0,0 +1,21 @@
+<#
+Version: 1.0
+Author: 
+- Jeroen Burgerhout (burgerhout.org)
+Script: Remove-WindowsBackup
+Description: Script removes the new Dev Home app on Windows 11 23H2.
+Hint: This is a community script. There is no guarantee for this. Please check thoroughly before running.
+Version 1.0: Init
+Run this script using the logged-on credentials: Yes
+Enforce script signature check: No
+Run script in 64-bit PowerShell: Yes
+#> 
+
+try{
+    Get-AppxPackage -Name *DevHome* | Remove-AppxPackage -ErrorAction stop
+    Write-Host "Dev Home successfully removed."
+
+}
+catch{
+    Write-Error "Error removing Dev Home."
+}
